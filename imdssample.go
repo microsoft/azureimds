72e5831866a7c6c44f91b107b8f7e319ef41842a
gaagar <gaagar@microsoft.com>
2017-06-14 10:06:21 -0700

    imds go sample

---
+++ imdssample.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+)
+
+func main() {
+    client := &http.Client{}
+
+    req, _ := http.NewRequest("GET", "http://169.254.169.254/metadata/instance", nil)
+    req.Header.Add("Metadata", "True")
+
+    q := req.URL.Query()
+    q.Add("format", "json")
+    q.Add("api-version", "2017-04-02")
+    req.URL.RawQuery = q.Encode()
+
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Errored when sending request to the server")
+        return
+    }
+
+    defer resp.Body.Close()
+    resp_body, _ := ioutil.ReadAll(resp.Body)
+
+    fmt.Println(resp.Status)
+    fmt.Println(string(resp_body))
+}
