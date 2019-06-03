04b58cd04a185f3a92ac4f9ed3c8562b638cca03
Rich Franklin <rifrankl@microsoft.com>
2019-06-03 15:15:00 -0700

    Updated Powershell sample with Attested

--- IMDSSample.ps1
+++ IMDSSample.ps1
@@ -1,1 +1,43 @@
-Invoke-RestMethod -Method GET -Uri http://169.254.169.254/metadata/instance?api-version=2017-04-02 -Headers @{"Metadata"="True"} | ConvertTo-JSON -Depth 99
+$ImdsServer = "http://169.254.169.254"
+$InstanceEndpoint = $ImdsServer + "/metadata/instance"
+$AttestedEndpoint = $ImdsServer + "/metadata/attested/document"
+
+function Query-InstanceEndpoint
+{
+    $uri = $InstanceEndpoint + "?api-version=2018-10-01"
+    $result = Invoke-RestMethod -Method GET -Uri $uri -Headers @{"Metadata"="True"}
+    return $result
+}
+
+function Query-AttestedEndpoint
+{
+    $uri = $AttestedEndpoint + "?api-version=2018-10-01"
+    $result = Invoke-RestMethod -Method GET -Uri $uri -Headers @{"Metadata"="True"}
+    return $result
+}
+
+function Parse-AttestedResponse
+{
+    param
+    (
+        [PSObject]$response
+    )
+
+    $signature = $response.signature
+    $decoded = [System.Convert]::FromBase64String($signature)
+    $cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]($decoded)
+    $chain = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Chain
+    $result = $chain.Build($cert)
+    foreach($element in $chain.ChainElements)
+    {
+        $element.Certificate | Format-List *
+    }
+}
+
+# Make Instance call and print the response
+$result = Query-InstanceEndpoint
+$result | ConvertTo-JSON -Depth 99
+
+# Make Attested call and parse the response
+$result = Query-AttestedEndpoint
+Parse-AttestedResponse $result
