2ce5eaa38840bbdd740e29dfa84b0db8b2d1b32d
Rich Franklin <rifrankl@microsoft.com>
2019-07-03 14:07:14 -0700

    Whitespace

--- imdssample.java
+++ imdssample.java
@@ -17,8 +17,8 @@ import com.google.gson.Gson;
 
 /**
  * This example has two dependencies:
- * 	1. Google Gson library: https://github.com/google/gson
- * 	2. BouncyCastle Java library: https://www.bouncycastle.org/java.html
+ *  1. Google Gson library: https://github.com/google/gson
+ *  2. BouncyCastle Java library: https://www.bouncycastle.org/java.html
  */
 public class IMDSSample 
 {
@@ -27,120 +27,120 @@ public class IMDSSample
     public static final String AttestedEndpoint = ImdsServer + "/metadata/attested/document";
     public static final String NonceValue = "123456";
     
-	public static void main(String[] args)
-	{
-		// Query /instance metadata
-		String result = QueryInstanceEndpoint();
+    public static void main(String[] args)
+    {
+        // Query /instance metadata
+        String result = QueryInstanceEndpoint();
         ParseInstanceResponse(result);
 
         // Make Attested call and parse the response
         result = QueryAttestedEndpoint();
         ParseAttestedResponse(result);
-	}
-	
-	private static void ParseInstanceResponse(String response)
-	{
-		System.out.println("Instance response: " + response);
-	}
-	
-	private static void ParseAttestedResponse(String response)
-	{
-		System.out.println("Parsing Attested response");
-		AttestedDocument document = new Gson().fromJson(response, AttestedDocument.class);
-		byte[] decoded = Base64.decode(document.signature);
-		ValidateCertificate(decoded);
-		ValidateAttestedData(decoded);
-	}
-	
-	private static void ValidateCertificate(byte[] decoded)
-	{
-		try
-		{
-			CertificateFactory factory =  CertificateFactory.getInstance("X.509");
-			Collection certs = factory.generateCertificates(new ByteArrayInputStream(decoded));
-			Iterator it = certs.iterator();
-			while(it.hasNext())
-			{
-				X509Certificate cert = (X509Certificate)it.next();
-				cert.checkValidity();
-				X500Principal issuer = cert.getIssuerX500Principal();
-				System.out.println("Issuer: " + issuer.toString());
-				X500Principal subject = cert.getSubjectX500Principal();
-				System.out.println("Subject: " + subject.toString());
-				System.out.println("Valid until: " + cert.getNotAfter().toString());
-			}
-		}
-		catch(Exception ex)
-		{
-			System.out.println("Exception validating certificate: " + ex.getMessage());
-		}
-	}
-	
-	private static void ValidateAttestedData(byte[] decoded)
-	{
-		try
-		{
-			CMSSignedData signature = new CMSSignedData(decoded);
-			CMSTypedData signedData = signature.getSignedContent();
-			String signedDataString = new String((byte[])signedData.getContent());
-			System.out.println("Attested data: " + signedDataString);
-			AttestedData data = new Gson().fromJson(signedDataString, AttestedData.class);
-			if(data.nonce.compareTo(NonceValue) == 0)
-			{
-				System.out.println("Nonce values match");
-			}
-		}
-		catch(Exception ex)
-		{
-			System.out.println("Exception validating data: " + ex.getMessage());
-		}
-	}
-	
-	private static String QueryInstanceEndpoint()
-	{
-		return QueryImds(InstanceEndpoint, "2017-04-02");		
-	}
-	
-	private static String QueryAttestedEndpoint()
+    }
+    
+    private static void ParseInstanceResponse(String response)
     {
-		String nonce = "nonce=" + NonceValue;
+        System.out.println("Instance response: " + response);
+    }
+    
+    private static void ParseAttestedResponse(String response)
+    {
+        System.out.println("Parsing Attested response");
+        AttestedDocument document = new Gson().fromJson(response, AttestedDocument.class);
+        byte[] decoded = Base64.decode(document.signature);
+        ValidateCertificate(decoded);
+        ValidateAttestedData(decoded);
+    }
+    
+    private static void ValidateCertificate(byte[] decoded)
+    {
+        try
+        {
+            CertificateFactory factory =  CertificateFactory.getInstance("X.509");
+            Collection certs = factory.generateCertificates(new ByteArrayInputStream(decoded));
+            Iterator it = certs.iterator();
+            while(it.hasNext())
+            {
+                X509Certificate cert = (X509Certificate)it.next();
+                cert.checkValidity();
+                X500Principal issuer = cert.getIssuerX500Principal();
+                System.out.println("Issuer: " + issuer.toString());
+                X500Principal subject = cert.getSubjectX500Principal();
+                System.out.println("Subject: " + subject.toString());
+                System.out.println("Valid until: " + cert.getNotAfter().toString());
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Exception validating certificate: " + ex.getMessage());
+        }
+    }
+    
+    private static void ValidateAttestedData(byte[] decoded)
+    {
+        try
+        {
+            CMSSignedData signature = new CMSSignedData(decoded);
+            CMSTypedData signedData = signature.getSignedContent();
+            String signedDataString = new String((byte[])signedData.getContent());
+            System.out.println("Attested data: " + signedDataString);
+            AttestedData data = new Gson().fromJson(signedDataString, AttestedData.class);
+            if(data.nonce.compareTo(NonceValue) == 0)
+            {
+                System.out.println("Nonce values match");
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Exception validating data: " + ex.getMessage());
+        }
+    }
+    
+    private static String QueryInstanceEndpoint()
+    {
+        return QueryImds(InstanceEndpoint, "2017-04-02");       
+    }
+    
+    private static String QueryAttestedEndpoint()
+    {
+        String nonce = "nonce=" + NonceValue;
         return QueryImds(AttestedEndpoint, "2019-04-30", nonce);
     }
-	
-	private static String QueryImds(String path, String apiVersion)
-	{
-		return QueryImds(path, apiVersion, "");
-	}
-	
-	private static String QueryImds(String path, String apiVersion, String otherParams)
-	{
-		String imdsUrl = path + "?api-version=" + apiVersion;
-		if(otherParams != null && !otherParams.isEmpty())
-		{
-			imdsUrl += "&" + otherParams;
-		}
-		
-		try
-		{
-			URL url = new URL(imdsUrl);
-		  	HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		  	con.setRequestMethod("GET");
-		  	con.setRequestProperty("Metadata", "True");
-		  	BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		  	String line;
-		  	StringBuffer response = new StringBuffer();
-		  	while ((line = in.readLine()) != null) {
-			  response.append(line);
-		  	}
-		  	in.close();
-		  	return response.toString();
-		}
-		catch(Exception ex)
-		{
-			return "";
-		}
-	}
-	
+    
+    private static String QueryImds(String path, String apiVersion)
+    {
+        return QueryImds(path, apiVersion, "");
+    }
+    
+    private static String QueryImds(String path, String apiVersion, String otherParams)
+    {
+        String imdsUrl = path + "?api-version=" + apiVersion;
+        if(otherParams != null && !otherParams.isEmpty())
+        {
+            imdsUrl += "&" + otherParams;
+        }
+        
+        try
+        {
+            URL url = new URL(imdsUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Metadata", "True");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = in.readLine()) != null) {
+              response.append(line);
+            }
+            in.close();
+            return response.toString();
+        }
+        catch(Exception ex)
+        {
+            return "";
+        }
+    }
+    
     public class AttestedDocument
     {
         public String encoding;
